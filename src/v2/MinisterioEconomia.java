692091043eb38d7637d6b6431ba1a24ca8657092
Cristian Gutierrez <cristian.gutierrez@despegar.com>
2019-02-19 10:09:35 -0300

    hola

--- src/v2/MinisterioEconomia.java
+++ src/v2/MinisterioEconomia.java
@@ -76,6 +76,7 @@ public class MinisterioEconomia {
     public static  void main(String... args) {
         System.out.println("Hi");
         System.out.println("Hello");
+        System.out.println("Hola!");
     }
 
 
